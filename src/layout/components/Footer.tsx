48c1bef2da4eb7b77b459058c7159277282d8b51
RulinRulon <lucaluriaud@gmail.com>
2024-09-05 20:05:08 -0300

    Avances en el footer

--- src/layout/components/Footer.tsx
+++ src/layout/components/Footer.tsx
@@ -1,23 +1,43 @@
 import * as React from "react";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import SearchIcon from "@mui/icons-material/Search";
-import MailOutlineIcon from "@mui/icons-material/MailOutline";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import FaceIcon from "@mui/icons-material/Face";
+import { Button, ButtonProps, Stack } from "@mui/material";
+import styled from "@emotion/styled";
 
 export default function LabelBottomNavigation() {
   const [value, setValue] = React.useState("recents");
 
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
+  const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
+    "&:hover": {
+      color: "#fff",
+      backgroundColor: "#486c99",
+    },
+  }));
 
   return (
     <footer>
-      <p>Contactanos</p>
-      <p>Instagram</p>
-      <p>Teléfono</p>
+      <Stack
+        sx={{
+          bgcolor: "#153f73",
+          alignItems: "center",
+          flexDirection: "row",
+          justifyContent: "space-around",
+          color: "#fff",
+          paddingBottom: 1,
+          paddingTop: 1,
+        }}
+      >
+        <ColorButton variant="text" sx={{ color: "white" }}>
+          Contactanos
+        </ColorButton>
+        <ColorButton variant="text" sx={{ color: "white" }}>
+          Nuestras redes
+        </ColorButton>
+        <ColorButton variant="text" sx={{ color: "white" }}>
+          Sobre nosotros
+        </ColorButton>
+        <ColorButton variant="text" sx={{ color: "white" }}>
+          Ayuda
+        </ColorButton>
+      </Stack>
     </footer>
   );
 }
