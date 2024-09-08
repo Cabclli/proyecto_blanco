29df8e2cd837351c27071f66774f98339e0cd2da
RulinRulon <lucaluriaud@gmail.com>
2024-09-08 00:21:57 -0300

    footer a medio terminar

--- src/layout/components/Footer.tsx
+++ src/layout/components/Footer.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { Button, ButtonProps, Stack } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Button, ButtonProps, Link, Stack } from "@mui/material";
 import styled from "@emotion/styled";
 
 export default function LabelBottomNavigation() {
   const [value, setValue] = React.useState("recents");
 
-  const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
+  const ColorButton = styled(Button)<ButtonProps>(({ }) => ({
     "&:hover": {
       color: "#fff",
       backgroundColor: "#486c99",
@@ -26,16 +26,56 @@ export default function LabelBottomNavigation() {
         }}
       >
         <ColorButton variant="text" sx={{ color: "white" }}>
-          Contactanos
+          <Accordion sx={{bgcolor:"transparent", boxShadow: 0, color:"#fff"}}>
+            <AccordionSummary
+              aria-controls="panel1-content"
+              id="panel1-header"
+            >
+              Contactanos 
+            </AccordionSummary>
+            <AccordionDetails>
+            <Link href="https://www.asrock.com/mb/AMD/A520M-HDV/index.la.asp#BIOS" color={"#fff"}>11-3227-8469</Link>
+            </AccordionDetails>
+          </Accordion>
         </ColorButton>
         <ColorButton variant="text" sx={{ color: "white" }}>
-          Nuestras redes
+          <Accordion sx={{bgcolor:"transparent", boxShadow: 0, color:"#fff"}}>
+            <AccordionSummary
+              aria-controls="panel1-content"
+              id="panel1-header"
+            >
+              Nuestras redes 
+            </AccordionSummary>
+            <AccordionDetails>
+            <Link href="https://www.instagram.com/lucaluriaud" color={"#fff"}>Instagram</Link>
+            </AccordionDetails>
+          </Accordion>
         </ColorButton>
         <ColorButton variant="text" sx={{ color: "white" }}>
-          Sobre nosotros
+          <Accordion sx={{bgcolor:"transparent", boxShadow: 0, color:"#fff"}}>
+            <AccordionSummary
+              aria-controls="panel1-content"
+              id="panel1-header"
+            >
+              Sobre nosotros 
+            </AccordionSummary>
+            <AccordionDetails>
+            <Link href="https://www.asrock.com/mb/AMD/A520M-HDV/index.la.asp#BIOS" color={"#fff"}>Hola, blablabla</Link>
+            </AccordionDetails>
+          </Accordion>
         </ColorButton>
         <ColorButton variant="text" sx={{ color: "white" }}>
-          Ayuda
+          <Accordion sx={{bgcolor:"transparent", boxShadow: 0, color:"#fff"}}>
+            <AccordionSummary
+              aria-controls="panel1-content"
+              id="panel1-header"
+            >
+              Ayuda 
+            </AccordionSummary>
+            <AccordionDetails>
+            <Link href="https://www.asrock.com/mb/AMD/A520M-HDV/index.la.asp#BIOS" color={"#fff"}>Problema, solucion</Link>
+            </AccordionDetails>
+          </Accordion>
         </ColorButton>
       </Stack>
     </footer>
