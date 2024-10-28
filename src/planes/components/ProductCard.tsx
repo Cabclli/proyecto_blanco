ea4d05a2d988af28dd8a84259ad6df212c3faef6
RulinRulon <lucaluriaud@gmail.com>
2024-10-28 13:12:05 -0300

    Avances en pagina componentes

--- src/planes/components/ProductCard.tsx
+++ src/planes/components/ProductCard.tsx
@@ -1,7 +1,8 @@
-import { Card, CardContent, CardHeader, Grid, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { Button, Card, CardContent, CardHeader, CardMedia, Grid, Typography } from "@mui/material";
+import { useRouter } from "next/router";
 import React from "react";
 
-// Define la interfaz para las propiedades del componente
 interface ProductCardProps {
   component: {
     name: string;
@@ -11,16 +12,41 @@ interface ProductCardProps {
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ component }) => {
+  const formattedPrice = `USD$ ${component.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+
+  const router = useRouter();
+
+  const handleCarritoClick = () => {
+    router.push("/carrito");
+  };
+
   return (
-    <Grid item xs={4}>
-      <Card sx={{ marginLeft: "10%" }}>
-        <CardHeader title={component.name} subheader={component.description} />
-        <CardContent>
-          <Typography variant="h4" className="title">
-            {`USD$ ${component.price
-              .toString()
-              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`}
+    <Grid item xs={3}>
+      <Card sx={{ margin: "10px", maxWidth: 350, borderRadius: "5%", display: "flex", flexDirection: "column" }}>
+        <div style={{ overflow: "hidden", borderRadius: "5%", maxHeight: "300px", maxWidth: "250px", margin: "0 auto" }}>
+          <CardMedia
+            component="img"
+            image="https://static.gigabyte.com/StaticFile/Image/Global/1f7a4b7372688a9959a997aa486252e1/Product/25956/Png"
+            sx={{ objectFit: "contain" }}
+          />
+        </div>
+        <CardHeader title={component.name} />
+        <CardContent sx={{ display: "flex", alignItems: "center" }}>
+          <Typography variant="h5" fontWeight="bold">
+            {formattedPrice}
           </Typography>
+          <Button
+            onClick={handleCarritoClick}
+            sx={{
+              marginLeft: "25%",
+              backgroundColor: "#285b99",
+              padding: "10px",
+              borderRadius: "10%",
+              "&:hover": { backgroundColor: "#1e4a76" },
+            }}
+          >
+            <ShoppingCart sx={{ color: "#fff" }} />
+          </Button>
         </CardContent>
       </Card>
     </Grid>
--- src/planes/components/ProductsDisplay.tsx
+++ src/planes/components/ProductsDisplay.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard"; // Ajusta la ruta según tu estructura
-import { Grid } from "@mui/material";
+import { Button, Grid, Stack, Typography } from "@mui/material";
+import { useRouter } from "next/router";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 interface ComponentData {
   id: number;
@@ -26,19 +28,40 @@ const ComponentList: React.FC = () => {
     fetchComponents();
   }, []);
 
+  const router = useRouter();
+  const handleVolverClick = () => {
+    router.push("/");
+  };
+
+
   return (
-    <Grid container spacing={2}>
-      {components.map((component) => (
-        <ProductCard
-          key={component.id}
-          component={{
-            name: component.name,
-            description: component.category.name,
-            price: component.price,
-          }}
-        />
-      ))}
-    </Grid>
+    <div>  
+      <Stack sx={{ display:"flex",
+        flexDirection:"row",
+        borderBottom:"3px solid gray",
+        marginLeft: "10%",
+        marginRight:"10%",
+        paddingTop: "2%",
+        alignItems:"center"
+        }}>
+        <Button onClick={handleVolverClick}>
+          <ArrowBackIosIcon/>
+        </Button>
+        <Typography variant="h4">Destacados</Typography>
+      </Stack>
+      <Grid container spacing={2}>
+        {components.map((component) => (
+         <ProductCard
+            key={component.id}
+            component={{
+              name: component.name,
+              description: component.category.name,
+              price: component.price,
+           }}
+          />
+        ))}
+      </Grid>
+    </div>
   );
 };
 
