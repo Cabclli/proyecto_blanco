faf981589827abc2748ffa1c73a7510fe45e4326
RulinRulon <lucaluriaud@gmail.com>
2024-12-12 19:03:27 -0300

    .

--- src/productos/components/ProductsDisplay.tsx
+++ src/productos/components/ProductsDisplay.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
-import { Button, Stack, Typography, MenuItem, Select, FormControl, InputLabel, SelectChangeEvent } from "@mui/material";
+import { Button, Stack, Typography, MenuItem, Select, FormControl, SelectChangeEvent, InputAdornment } from "@mui/material";
 import { useRouter } from "next/router";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import styles from './ProductDisplay.module.css';
@@ -60,20 +60,34 @@ const ComponentList: React.FC = () => {
 
         <Stack className={styles.filtro}>
           <FormControl sx={{
-                borderBottom: "2px solid #285b99",
-                height: "45px",
-                width: "100%",
-                ".MuiOutlinedInput-notchedOutline": {
-                  border: "none",
-                },
-              }}>
-            <InputLabel id="sort-label">Ordenar por precio</InputLabel>
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            borderBottom : "2px solid #285b99",
+            height: "45px",
+            width: "400px",
+            ".MuiOutlinedInput-notchedOutline": {
+              border: "none",
+            },
+          }}>
             <Select
               value={sortOrder}
               onChange={handleSortChange}
-              label="Ordenar por precio"
+              sx={{
+                display: "flex",
+                alignItems: "baseline",
+                width: "100%",
+                padding: "0px",
+              }}
+              MenuProps={{
+                PaperProps: {
+                  style: {
+                    maxHeight: 300,
+                  },
+                },
+              }}
             >
-              <MenuItem value="">Sin ordenar</MenuItem>
+              <MenuItem value="Destacados">Destacados</MenuItem>
               <MenuItem value="asc">De menor a mayor</MenuItem>
               <MenuItem value="desc">De mayor a menor</MenuItem>
             </Select>
